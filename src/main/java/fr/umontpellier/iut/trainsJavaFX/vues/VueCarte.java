5046949c52fe134e8665810b29ef04be16b76543
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-06-13 17:21:28 +0200

    Implémentation du binding de la réserve & Implémentation affichage nombre de cartes restantes (via binding)

--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueCarte.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueCarte.java
@@ -1,10 +1,17 @@
 package fr.umontpellier.iut.trainsJavaFX.vues;
 
 import fr.umontpellier.iut.trainsJavaFX.ICarte;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
 import javafx.event.EventHandler;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import java.text.Normalizer;
 
 /**
  * Cette classe représente la vue d'une carte.
@@ -16,17 +23,40 @@ public class VueCarte extends StackPane {
     private final ICarte carte;
     private final ImageView imageView;
 
+    private final int ratioRedimensionnage = 5;
+
     public VueCarte(ICarte carte) {
         this.carte = carte;
+        String nomCarte = Normalizer.normalize(carte.getNom().replaceAll(" ", "_").toLowerCase(), Normalizer.Form.NFKD).replaceAll("\\p{M}", "");
         imageView = new ImageView(String.valueOf(getClass().getClassLoader()
-                .getResource("images/cartes/" + carte.getNom().replaceAll(" ", "_").toLowerCase() + ".jpg")));
-        imageView.setFitWidth(imageView.getImage().getWidth() / 5);
-        imageView.setFitHeight(imageView.getImage().getHeight() / 5);
+                .getResource("images/cartes/" + nomCarte + ".jpg")));
+        imageView.setFitWidth(imageView.getImage().getWidth() / ratioRedimensionnage);
+        imageView.setFitHeight(imageView.getImage().getHeight() / ratioRedimensionnage);
         getChildren().add(imageView);
     }
 
     public void setCarteChoisieListener(EventHandler<MouseEvent> quandCarteEstChoisie) {
         setOnMouseClicked(quandCarteEstChoisie);
+    }
+
+    public void setNbCartes(IntegerProperty nbCartesProperty) {
+        // Suppression de l'ancien StackPane si déjà existant
+        getChildren().stream().filter(node -> node instanceof StackPane).findFirst().ifPresent(node -> getChildren().remove(node));
+        StackPane conteneur = new StackPane();
+        conteneur.translateXProperty().bind(Bindings.multiply((double) 25 / 75, imageView.fitWidthProperty()));
+        conteneur.translateYProperty().bind(Bindings.multiply((double) 40 / 75, imageView.fitWidthProperty()));
+
+        Circle fondNbCartes = new Circle(0, Color.WHITE);
+        fondNbCartes.radiusProperty().bind(Bindings.multiply((double) 10 / 75, imageView.fitWidthProperty()));
+        fondNbCartes.setStroke(Color.BLACK);
+        Label nbCartes = new Label();
+        nbCartes.textProperty().bind(Bindings.convert(nbCartesProperty));
+        nbCartes.styleProperty().bind(Bindings.concat("-fx-font-size: ", Bindings.multiply((double) 16 / 75, imageView.fitWidthProperty())));
+        conteneur.getChildren().addAll(fondNbCartes, nbCartes);
+        getChildren().add(conteneur);
+
+        // Désactivation de la carte si nbCartes est égale à 0
+        disableProperty().bind(nbCartesProperty.isEqualTo(0));
     }
 
     public ICarte getCarte() {
--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueReserve.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueReserve.java
@@ -1,11 +1,22 @@
 package fr.umontpellier.iut.trainsJavaFX.vues;
 
+import fr.umontpellier.iut.trainsJavaFX.GestionJeu;
+import fr.umontpellier.iut.trainsJavaFX.IJeu;
+import javafx.beans.property.IntegerProperty;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class VueReserve extends VBox {
+    @FXML
+    private FlowPane cartesReserve;
+
     public VueReserve() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/reserve.fxml"));
@@ -15,5 +26,18 @@ public class VueReserve extends VBox {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        cartesReserve.getChildren().clear();
+        IJeu jeu = GestionJeu.getJeu();
+        for(Map.Entry<String, IntegerProperty> entry : jeu.getTaillesPilesReserveProperties().entrySet()) {
+            VueCarte carte = new VueCarte(jeu.getReserve().getCarte(entry.getKey()));
+            carte.setCarteChoisieListener(actionChoisirCarte);
+            carte.setNbCartes(entry.getValue());
+            cartesReserve.getChildren().add(carte);
+        }
     }
+
+    EventHandler<MouseEvent> actionChoisirCarte = (mouseEvent -> {
+        VueCarte vueCarte = (VueCarte) mouseEvent.getSource();
+        GestionJeu.getJeu().uneCarteDeLaReserveEstAchetee(vueCarte.getCarte().getNom());
+    });
 }
