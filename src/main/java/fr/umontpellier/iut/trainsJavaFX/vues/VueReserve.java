fa906ee40b8a4a31f72bcd28e1c6b37903ae5ac8
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-06-17 03:02:15 +0200

    Implémentation de tous les inputs des cartes actions possibles & Suppression des cartes écartées dans l'interface graphique

--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueReserve.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueReserve.java
@@ -4,19 +4,13 @@ import fr.umontpellier.iut.trainsJavaFX.GestionJeu;
 import fr.umontpellier.iut.trainsJavaFX.IJeu;
 import javafx.beans.property.IntegerProperty;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
-
 import java.io.IOException;
 import java.util.Map;
 
-public class VueReserve extends VBox {
-    @FXML
-    private FlowPane cartesReserve;
-
+public class VueReserve extends FlowPane {
     public VueReserve() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/reserve.fxml"));
@@ -26,13 +20,13 @@ public class VueReserve extends VBox {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        cartesReserve.getChildren().clear();
+        getChildren().clear();
         IJeu jeu = GestionJeu.getJeu();
         for(Map.Entry<String, IntegerProperty> entry : jeu.getTaillesPilesReserveProperties().entrySet()) {
             VueCarte carte = new VueCarte(jeu.getReserve().getCarte(entry.getKey()));
             carte.setCarteChoisieListener(actionChoisirCarte);
             carte.setNbCartes(entry.getValue());
-            cartesReserve.getChildren().add(carte);
+            getChildren().add(carte);
         }
     }
 
